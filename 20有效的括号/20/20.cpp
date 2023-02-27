739a5de11af6e0331e81181eea54a03c65a1d5cb
kuanlun <75818689+kuanlun@users.noreply.github.com>
2023-02-27 21:19:36 +0800

    Add files via upload

---
+++ 20有效的括号/20/20.cpp
@@ -0,0 +1,34 @@
+﻿// 20.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<unordered_map>
+#include<stack>
+using namespace std;
+class Solution {
+public:
+	bool isValid(string s) {
+		unordered_map<char, char> pair = { {'(',')'} , {'[',']'} , {'{','}'} };
+		stack<char> stt;
+		for (int i = 0;i < s.length();i++)
+		{
+			if (pair.find(s[i]) != pair.end())
+			{
+				stt.push(pair[s[i]]);
+			}
+			else
+			{
+				if (stt.empty() || stt.top() != s[i])
+				{
+					return false;
+				}
+				stt.pop();
+			}
+		}
+		return stt.empty();
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
